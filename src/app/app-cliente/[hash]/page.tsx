de796b5988dca575514d7aab2296af48d1f3aca1
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-06-16 13:15:11 -0300

    sinc

--- src/app/app-cliente/[hash]/page.tsx
+++ src/app/app-cliente/[hash]/page.tsx
@@ -1,10 +1,5 @@
 import AppClienteContent from "@/lib/features/app-cliente/components/app-cliente-content";
 import { clienteService } from "@/lib/features/app-cliente/services/cliente-service";
-import MonitorContent from "@/lib/features/monitor/components/monitor-content";
-import { ConfiguracaoProvider } from "@/lib/features/monitor/context/configuracao-context";
-import { FilaProvider } from "@/lib/features/monitor/context/fila-context";
-import { configuracaoService } from "@/lib/features/monitor/services/configuracao-service";
-import { filaService } from "@/lib/features/monitor/services/fila-service";
 
 interface AppUsuarioPageProps {
   params: { hash: string };
--- src/lib/features/app-cliente/components/app-cliente-content.tsx
+++ src/lib/features/app-cliente/components/app-cliente-content.tsx
@@ -16,6 +16,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import ChamadoAtual from "../../monitor/components/chamado-atual";
 import { useSignalrAppCliente } from "../hooks/use-signalr-app-cliente";
 import useAppCliente from "../hooks/use-app-cliente";
+import { Geist } from "next/font/google";
 
 interface AppUsuarioContentProps {
   dadosIniciasCliente: DadosIniciaisClienteDTO;
@@ -88,7 +89,7 @@ export default function AppClienteContent({
                     animate={{ opacity: 1 }}
                     exit={{ opacity: 0 }}
                     transition={{ duration: 0.5 }}
-                    className="text-[15em] text-center font-extrabold leading-none"
+                    className={`font-(family-name:--font-geist-sans) text-[15em] text-center font-extrabold leading-none`}
                     style={{
                       color: configuracao.corPrimaria,
                     }}
