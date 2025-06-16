35fb3e61833bc62431d8a3078f4455eb8951e762
Nicolas Tomasi <tomasitrabalho@gmail.com>
2025-06-16 11:09:29 -0300

    sinc

--- src/lib/features/monitor/components/chamado-atual.tsx
+++ src/lib/features/monitor/components/chamado-atual.tsx
@@ -27,7 +27,7 @@ export default function ChamadoAtual({ ultimosChamados }: ChamadoAtualProps) {
         </p>
       </div>
 
-      <Card className="flex-1 w-full rounded-[1.5em] flex flex-col justify-center items-center p-[4em] shadow-[0_0_10px_rgba(0,0,0,0.1)] border-none">
+      <Card className="flex-1 w-full rounded-[1.5em] flex flex-col justify-center items-center  shadow-[0_0_10px_rgba(0,0,0,0.1)] border-none">
         {ultimosChamados[0]?.nome && (
           <motion.p
             key={ultimosChamados[0].nome}
